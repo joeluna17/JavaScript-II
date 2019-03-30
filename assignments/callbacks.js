b48ec29a8bc393438173ae1fbdd9eb278381d885
Joseph Luna <josephluna@Josephs-MBP.local>
2019-03-29 22:34:08 -0500

    committing complete array-method.js plus stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
   //Given this problem: 
   
@@ -27,29 +27,63 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length)
 }
+
+console.log(getLength(items, length => length))
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+console.log(last(items, item => item))
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  const sum = x+y
+    return cb(sum)
+      }
+    console.log(sumNums(2,3,sum => sum))
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x*y
+  return cb(product)
 }
+
+console.log(multiplyNums(6,9, prod => prod))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
+  let result = false // note self we had to use a var to store the result in, compiler will not return form scope result of conditional statement. Only return undifined because of callback patter, like swift we must assign to outerscope if cb.
+  list.forEach(element => {
+    if(element === item){   
+      result = true
+    }
+      else{
+        reslut = false
+      }
+  })
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(result)
 }
+console.log(contains('yo-yo', items, isPresent => isPresent))
 
 /* STRETCH PROBLEM */
 
+let someArray = ["spacejam", "robin hood", "jack and the beanstock", "bambi", "robin hood", "spacejam", "lion king"]
+
 function removeDuplicates(array, cb) {
+
+  let reducedArray = array.reduce((a,b) => {
+      if (a.indexOf(b)<0)a.push(b)
+      return a
+  },[])
+  return cb(reducedArray)
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+console.log(removeDuplicates(someArray, item => item))
